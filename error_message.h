52ab8082b55335c27490127f8df335f02dd902e9
Alex Buchkin <alexbuchkin96@yandex.ru>
2022-04-02 23:01:39 +0300

    add print mutex

--- error_message.h
+++ error_message.h
@@ -1,18 +1,29 @@
 #pragma once
 
+#include <cstdio>
 #include <errno.h>
 #include <iostream>
+#include <mutex>
 #include <sstream>
-#include <cstdio>
+
+namespace {
+	
+std::mutex printMutex;	
+
+} // namespace
 
 #define PRINT_ERROR_MESSAGE(msg) \
 do {  \
-	std::cerr << __FILE__ << ":" << __LINE__ << "   " << (msg) << std::endl;  \
+	std::ostringstream out;  \
+	out << __FILE__ << ":" << __LINE__ << "   " << (msg);  \
+	const std::lock_guard lock(printMutex);  \
+	std::cerr << out.str() << std::endl;  \
 } while(0);
 
 #define PRINT_PERROR_MESSAGE(msg) \
 do {  \
 	std::ostringstream out;  \
-	out << __FILE__ << ":" << __LINE__ << "   " << (msg) << std::endl;  \
+	out << __FILE__ << ":" << __LINE__ << "   " << (msg);  \
+	const std::lock_guard lock(printMutex);  \
 	perror(out.str().c_str());  \
 } while(0);
